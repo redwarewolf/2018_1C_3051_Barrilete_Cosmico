b4c5f5386a2f4492bc0914fc90e686f35c24a5c7
Redwarewolf <redwarewolf@gmail.com>
2018-04-25 00:10:10 -0300

    WIP

---
+++ TGC.Group/Model/AI/PatrolObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGC.Core.Mathematica;
+
+namespace TGC.Group.Model.AI
+{
+    class PatrolObject
+    {
+        private List<TGCVector3> positions;
+        private int currentPosition = 0;
+
+        public void update()
+        {
+
+        }
+
+        void patrolLogic()
+        {
+
+        }
+
+        void moveTo()
+        {
+
+        }
+    }
+}
